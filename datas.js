16406e888db7d2aad70444e3b9bbdc9aea21ee99
Grégory Lejeune <lejeune.greg@free.fr>
2020-09-29 16:32:59 +0200

    Change datas

--- datas.js
+++ datas.js
@@ -73,6 +73,48 @@ var RANGES = [
               }
             ]
           },
+          "BTN vs LJ Open": {
+            "categories": [
+              {
+                "name": "3bet",
+                "combos": "",
+                "color": "bg-orange-500"
+              },
+              {
+                "name": "Call",
+                "combos": "",
+                "color": "bg-green-500"
+              }
+            ]
+          },
+          "BTN vs HJ Open": {
+            "categories": [
+              {
+                "name": "3bet",
+                "combos": "",
+                "color": "bg-orange-500"
+              },
+              {
+                "name": "Call",
+                "combos": "",
+                "color": "bg-green-500"
+              }
+            ]
+          },
+          "BTN vs CO Open": {
+            "categories": [
+              {
+                "name": "3bet",
+                "combos": "AA,KK,QQ,JJ,TT:0.75,99:0.5,88:0.5,77:0.5,66:0.5,55:0.25,44:0.25,AK,AQs,AQo:0.75,AJs:0.5,AJo,ATs:0.5,ATo:0.75,A9s:0.5,A8s:0.75,A7s:0.5,A5s:0.75,A4s:0.75,A3s:0.75,KQ:0.75,KJs:0.75,KJo:0.5,KTs:0.5,K9s:0.5,QJs:0.75,QJo:0.5,QTs:0.5,Q9s:0.75,JTs:0.75,T9s:0.25,76s:0.25,65s:0.25,54s:0.25",
+                "color": "bg-orange-500"
+              },
+              {
+                "name": "Call",
+                "combos": "TT:0.25,99:0.5,88:0.5,77:0.5,66:0.5,55:0.25,44:0.25,AQo:0.25,AJs:0.5,ATs:0.5,A9s:0.5,A8s:0.25,A7s:0.5,A5s:0.25,A4s:0.25,A3s:0.25,KQ:0.25,KJ:0.25,KTs:0.5,K9s:0.5,QJs:0.25,QTs:0.5,JTs:0.25,T9s:0.25,76s:0.25,65s:0.25,54s:0.25",
+                "color": "bg-green-500"
+              }
+            ]
+          },
           "SB Open": {
             "categories": [
               {
@@ -82,16 +124,52 @@ var RANGES = [
               }
             ]
           },
+          "SB vs LJ Open": {
+            "categories": [
+              {
+                "name": "3bet",
+                "combos": "AA,KK,QQ,JJ,TT,99:0.5,88:0.5,77:0.25,AK,AQs,AQo:0.5,AJs,ATs,A5s:0.75,A4s:0.25,KQs,KJs,KTs:0.75,QJs,QTs:0.75,JTs:0.25,76s:0.25,65s:0.25,54s:0.25",
+                "color": "bg-orange-500"
+              }
+            ]
+          },
+          "SB vs HJ Open": {
+            "categories": [
+              {
+                "name": "3bet*",
+                "combos": "AA,KK,QQ,JJ,TT,99:0.5,88:0.5,77:0.25,AK,AQs,AQo:0.5,AJs,ATs,A5s:0.75,A4s:0.25,KQs,KJs,KTs:0.75,QJs,QTs:0.75,JTs:0.25,76s:0.25,65s:0.25,54s:0.25",
+                "color": "bg-orange-500"
+              }
+            ]
+          },
+          "SB vs CO Open": {
+            "categories": [
+              {
+                "name": "3bet*",
+                "combos": "AA,KK,QQ,JJ,TT,99:0.5,88:0.5,77:0.25,AK,AQs,AQo:0.5,AJs,ATs,A5s:0.75,A4s:0.25,KQs,KJs,KTs:0.75,QJs,QTs:0.75,JTs:0.25,76s:0.25,65s:0.25,54s:0.25",
+                "color": "bg-orange-500"
+              }
+            ]
+          },
+          "SB vs BTN Open": {
+            "categories": [
+              {
+                "name": "3bet",
+                "combos": "AA,KK,QQ,JJ,TT,99,88,77,66,55,AK,AQ,AJ,ATs,ATo:0.25,A9s,A8s,A7s:0.75,A5s,A4s,A3s:0.5,KQ,KJs,KJo:0.5,KTs,KTo:0.25,K9s,K8s:0.25,QJs,QTs,Q9s,JTs,J9s,T9s,T8s:0.75,98s:0.25,87s:0.5,76s:0.5,65s:0.25",
+                "color": "bg-orange-500"
+              }
+            ]
+          },
           "BB vs LJ Open": {
             "categories": [
               {
                 "name": "3bet",
-                "combos": "",
+                "combos": "AA,KK,QQ,JJ:0.5,AKs,AKo:0.75,AQs,AJs:0.5,ATs:0.5,A8s:0.25,A7s:0.25,A6s:0.25,A5s:0.25,A4s:0.25,KQs,KJs:0.75,KTs:0.25,K7s:0.25,K6s:0.25,K5s:0.25,QJs,QTs:0.5,Q9s:0.25,JTs:0.25,J9s:0.25,J8s:0.25,T9s:0.25,T8s:0.25,98s:0.25,87s:0.25,86s:0.25,76s:0.25,65s:0.25,54s:0.25",
                 "color": "bg-orange-500"
               },
               {
                 "name": "Call",
-                "combos": "",
+                "combos": "JJ:0.5,TT,99,88,77,66,55,44,33,22,AKo:0.25,AQo,AJs:0.5,AJo,ATs:0.5,ATo,A9s,A8s:0.75,A7s:0.75,A6s:0.75,A5s:0.75,A4s:0.75,A3s,A2s,KQo,KJs:0.25,KJo,KTs:0.75,KTo:0.25,K9s,K8s,K7s:0.75,K6s:0.75,K5s:0.75,K4s,K3s:0.75,K2s:0.5,QJo:0.5,QT:0.5,Q9s:0.75,Q8s,Q7s:0.5,Q6s:0.5,Q5s:0.5,JTs:0.75,J9s:0.75,J8s:0.75,T9s:0.75,T8s:0.75,T7s,98s:0.75,97s,96s,87s:0.75,86s:0.75,85s:0.5,76s:0.75,75s,74s:0.5,65s:0.75,64s,63s:0.5,54s:0.75,53s,52s:0.5,43s,42s:0.5",
                 "color": "bg-green-500"
               }
             ]
@@ -100,12 +178,12 @@ var RANGES = [
             "categories": [
               {
                 "name": "3bet",
-                "combos": "",
+                "combos": "AA,KK,QQ,JJ:0.75,TT:0.25,77:0.25,AK,AQs,AQo:0.25,AJs:0.5,ATs:0.5,A8s:0.25,A7s:0.25,A6s:0.25,A5s:0.25,A4s:0.25,A3s:0.25,KQs,KJs,KTs:0.75,K7s:0.25,K6s:0.25,K5s:0.25,K4s:0.25,QJs,QTs:0.75,Q9s:0.5,Q8s:0.5,JTs:0.75,J9s:0.5,T9s:0.25,T7s:0.25,97s:0.25,87s:0.25,76s:0.25,75s:0.25,65s:0.25,64s:0.25,54s:0.25",
                 "color": "bg-orange-500"
               },
               {
                 "name": "Call",
-                "combos": "",
+                "combos": "JJ:0.25,TT:0.75,99,88,77:0.75,66,55,44,33,22,AQo:0.75,AJs:0.5,AJo,ATs:0.5,ATo,A9s,A9o:0.5,A8s:0.75,A7s:0.75,A6s:0.75,A5s:0.75,A4s:0.75,A3s:0.75,A2s,KQo,KJo,KTs:0.25,KTo:0.5,K9s,K8s,K7s:0.75,K6s:0.75,K5s:0.75,K4s:0.75,K3s,K2s,QJo,QTs:0.25,QTo:0.5,Q9s:0.5,Q8s:0.5,Q7s:0.75,Q6s:0.75,Q5s:0.5,Q4s:0.5,JTs:0.25,JTo:0.5,J9s:0.5,J8s,J7s:0.5,T9s:0.75,T8s,T7s:0.75,98s,97s:0.75,96s,87s:0.75,86s,85s,76s:0.75,75s:0.75,74s,65s:0.75,64s:0.75,54s:0.75,53s,52s:0.5,43s,42s:0.5",
                 "color": "bg-green-500"
               }
             ]
@@ -114,12 +192,12 @@ var RANGES = [
             "categories": [
               {
                 "name": "3bet",
-                "combos": "",
+                "combos": "AA,KK,QQ,JJ,TT:0.75,99:0.25,88:0.13,77:0.13,66:0.13,AK,AQs,AQo:0.5,AJs,AJo:0.13,ATs:0.5,ATo:0.13,A9s:0.5,A8s:0.25,A7s:0.13,A6s:0.13,A5s:0.75,A4s:0.5,KQs,KQo:0.13,KJs:0.75,KJo:0.13,KTs,KTo:0.13,K9s:0.25,K7s:0.13,K6s:0.13,K5s:0.13,K4s:0.13,QJs,QJo:0.13,QTs,QTo:0.13,Q9s:0.5,Q8s:0.13,Q6s:0.13,Q5s:0.13,JTs,J9s:0.5,J8s:0.13,J7s:0.13,T9s:0.5,T8s:0.13,T7s:0.13,98s:0.13,97s:0.13,87s:0.25,86s:0.13,76s:0.25,75s:0.13,65s:0.5,64s:0.13,54s:0.5",
                 "color": "bg-orange-500"
               },
               {
                 "name": "Call",
-                "combos": "",
+                "combos": "TT:0.25,99:0.75,88:0.87,77:0.87,66:0.87,55,44,33,22,AQo:0.5,AJo:0.87,ATs:0.5,ATo:0.87,A9s:0.5,A9o,A8s:0.75,A8o:0.25,A7s:0.87,A6s:0.87,A5:0.25,A4s:0.5,A3s,A2s,KQo:0.87,KJs:0.25,KJo:0.87,KTo:0.87,K9s:0.75,K8s,K7s:0.87,K6s:0.87,K5s:0.87,K4s:0.87,K3s,K2s,QJo:0.87,QTo:0.87,Q9s:0.5,Q8s:0.87,Q7s,Q6s:0.87,Q5s:0.87,Q4s,Q3s:0.5,Q2s:0.5,J9s:0.5,J8s:0.87,J7s:0.87,J6s:0.5,J5s:0.5,T9s:0.5,T9o:0.25,T8s:0.87,T7s:0.87,98s:0.87,97s:0.87,96s,87s:0.75,86s:0.87,85s,76s:0.75,75s:0.87,74s,65s:0.5,64s:0.87,63s:0.5,54s:0.5,53s,52s:0.5,43s,42s:0.5",
                 "color": "bg-green-500"
               }
             ]
